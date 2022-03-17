2a3a2579a145c8d675752ea2b48da10ba676740c
zyx <2647880543@qq.com>
2022-03-17 13:50:24 +0800

    change

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div>
-      <home-header></home-header>
+      <home-header />
+      <home-swiper />
     </div>
 </template>
   
 <script setup lang='ts'>
 import HomeHeader from './components/Header.vue'
+import HomeSwiper from './components/Swiper.vue'
   export default{
     name:'App',
     components:{
-      HomeHeader
+      HomeHeader,
+      HomeSwiper
     }
   }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -6,9 +7,10 @@ import store from './store'
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
 import './assets/styles/iconfont.css'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
-
+Vue.use(VueAwesomeSwiper)
 new Vue({
   router,
   store,
